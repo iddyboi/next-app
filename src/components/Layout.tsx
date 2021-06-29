b8d9f69e7737c92c395aebefbfdf30290e5d35dd
idris <idrist2013@gmail.com>
2021-06-29 13:29:18 +0100

    re constructor of navigation header

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -2,11 +2,16 @@ import Link from "next/link";
 import styled from "styled-components";
 import { Container } from "../../styles/utility-styles";
 import { Subtitle, Title } from "../../styles/fonts";
-import React from "react";
+import Button from "../components/UI/Button";
 
 const NavLinks = styled.ul`
   display: flex;
+  align-items: center;
   font-size: ${({ theme }) => theme.fonts.md};
+  padding: 0.3rem 0;
+  @media (min-width: 768px) {
+    padding: 0 0;
+  }
   li {
     font-size: 1rem;
     @media (min-width: 768px) {
@@ -16,7 +21,7 @@ const NavLinks = styled.ul`
   a {
     margin-right: 10px;
     text-decoration: none;
-    color: ${({ theme }) => theme.colors.primary};
+    color: ${({ theme }) => theme.colors.white};
     &:hover {
       color: #ee0606;
     }
@@ -25,14 +30,32 @@ const NavLinks = styled.ul`
 const HeaderContainer = styled(Container)`
   margin-bottom: 50px;
   padding-top: 20px;
+  @media (min-width: 768px) {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+  }
+
+  h1 {
+    color: ${({ theme }) => theme.colors.white};
+  }
+
+  h2 {
+    color: ${({ theme }) => theme.colors.white};
+  }
+`;
+const HeaderWrapper = styled.header`
+  background-color: ${({ theme }) => theme.colors.primary};
 `;
 
 function Layout() {
   return (
-    <>
+    <HeaderWrapper>
       <HeaderContainer>
-        <Title fontSize="3rem">Uni-Go</Title>
-        <Subtitle>Find your Passion</Subtitle>
+        <div>
+          <Title fontSize="3rem">Uni-Go</Title>
+          <Subtitle>Find your Passion</Subtitle>
+        </div>
         <NavLinks>
           <li>
             <Link href="/">Home</Link>
@@ -49,9 +72,10 @@ function Layout() {
             {" "}
             <Link href="/login">Login</Link>
           </li>
+          <Button>Learn More</Button>
         </NavLinks>
       </HeaderContainer>
-    </>
+    </HeaderWrapper>
   );
 }
 
